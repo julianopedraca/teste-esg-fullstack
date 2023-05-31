c61c1c27c45f4b8120fdce0f3f8d4ba3e3217f93
julianopedraca <juliano.pedraca@gmail.com>
2023-05-31 15:15:38 -0300

    calculadora de desconto csharp

---
+++ csharp/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+const double desconto = 60.0/100;
+double produtoComDesconto;
+double valorDesconto;
+
+Console.WriteLine("Qual o valor do produto desejado");
+
+double valorProduto = Convert.ToDouble(Console.ReadLine());
+
+produtoComDesconto = Math.Round(valorProduto - (valorProduto*desconto),2);
+valorDesconto = Math.Round(valorProduto*desconto,2);
+
+Console.WriteLine($"Produto Custava: {valorProduto}");
+Console.WriteLine($"Produto com o desconto: {produtoComDesconto}");
+Console.WriteLine($"Valor do desconto: {valorDesconto}");
---
+++ csharp/README.md
@@ -0,0 +1,15 @@
+# Calculadora de Desconto
+
+Essa aplicação de console calcula o valor do produto com desconto, sem desconto e o valor do desconto dado na Black Friday.
+
+## Pré-requisito
+
+- [.NET Core SDK](https://dotnet.microsoft.com/download).
+
+## Como Executar
+
+Na pasta onde está o arquivo Program.cs, executar o seguinte comando:
+
+```
+dotnet run
+```
